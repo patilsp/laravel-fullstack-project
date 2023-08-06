5357361cdac5203adc88dea87fdd9abaaff73a64
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-08-06 16:35:15 +0530

    login and welcome screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,207 +19,69 @@
     <link href="assets/css/main.css" rel="stylesheet" type="text/css" />
   </head>
 
-  <style>
-    .page-bg {
-        background-size: cover;
-        background-attachment: fixed;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-image: url('assets/media/images/page-bg.jpg');
-    }
-   .card {
-      z-index: 999;
-      border-radius: 10px;
-    }
-    .page{
-      z-index: 999;
-    }
-    .menu{
-      z-index: 999;
-    }
-    .sidebar {
-      z-index: 999;
-    }
-    .flex-root {
-      width: 100%;
-      position: absolute;
-    }
-
-    .circles {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-    }
-
-    .circles li {
-      position: absolute;
-      display: block;
-      list-style: none;
-      width: 20px;
-      height: 20px;
-      background: rgba(255, 255, 255, 0.2);
-      animation: animate 25s linear infinite;
-      bottom: -150px;
-    }
-
-    .circles li:nth-child(1) {
-      left: 25%;
-      width: 80px;
-      height: 80px;
-      animation-delay: 0s;
-    }
-
-    .circles li:nth-child(2) {
-      left: 10%;
-      width: 20px;
-      height: 20px;
-      animation-delay: 2s;
-      animation-duration: 12s;
-    }
-
-    .circles li:nth-child(3) {
-      left: 70%;
-      width: 20px;
-      height: 20px;
-      animation-delay: 4s;
-    }
-
-    .circles li:nth-child(4) {
-      left: 40%;
-      width: 60px;
-      height: 60px;
-      animation-delay: 0s;
-      animation-duration: 18s;
-    }
-
-    .circles li:nth-child(5) {
-      left: 65%;
-      width: 20px;
-      height: 20px;
-      animation-delay: 0s;
-    }
-
-    .circles li:nth-child(6) {
-      left: 75%;
-      width: 110px;
-      height: 110px;
-      animation-delay: 3s;
-    }
-
-    .circles li:nth-child(7) {
-      left: 35%;
-      width: 150px;
-      height: 150px;
-      animation-delay: 7s;
-    }
-
-    .circles li:nth-child(8) {
-      left: 50%;
-      width: 25px;
-      height: 25px;
-      animation-delay: 15s;
-      animation-duration: 45s;
-    }
-
-    .circles li:nth-child(9) {
-      left: 20%;
-      width: 15px;
-      height: 15px;
-      animation-delay: 2s;
-      animation-duration: 35s;
-    }
-
-    .circles li:nth-child(10) {
-      left: 85%;
-      width: 150px;
-      height: 150px;
-      animation-delay: 0s;
-      animation-duration: 11s;
-    }
-
-    @keyframes animate {
-      0% {
-        transform: translateY(0) rotate(0deg);
-        opacity: 1;
-        border-radius: 0;
-      }
-
-      100% {
-        transform: translateY(-1000px) rotate(720deg);
-        opacity: 0;
-        border-radius: 50%;
-      }
-    }
-    .d_flex{
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-    #kt_page{
-        z-index: 9999;
-    }
-
-  </style>
-
-  <body id="kt_body" class="page-bg">
+  <body id="kt_body" class="app-blank">
     <script>
-      var defaultThemeMode = "dark";
-      var themeMode;
+        var defaultThemeMode = "light";
+        var themeMode;
 
-      if (document.documentElement) {
-        if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
-          themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
-        } else {
-          if (localStorage.getItem("data-bs-theme") !== null) {
-            themeMode = localStorage.getItem("data-bs-theme");
-          } else {
-            themeMode = defaultThemeMode;
-          }
+        if (document.documentElement) {
+            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+            } else {
+                if (localStorage.getItem("data-bs-theme") !== null) {
+                    themeMode = localStorage.getItem("data-bs-theme");
+                } else {
+                    themeMode = defaultThemeMode;
+                }
+            }
+
+            if (themeMode === "system") {
+                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
+
+            document.documentElement.setAttribute("data-bs-theme", themeMode);
         }
-
-        if (themeMode === "system") {
-          themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-        }
-
-        document.documentElement.setAttribute("data-bs-theme", themeMode);
-      }
     </script>
-    <!-- Start Page Loading -->
+     <div class="d-flex flex-column flex-root" id="kt_app_root">
+        <div class="d-flex flex-column flex-lg-row flex-column-fluid">
 
-    <!-- End Page Loading -->
-    <div class="d-flex flex-column flex-root">
-      <div class="d-flex flex-row flex-column-fluid me-lg-5" id="kt_page">
-        <div class="flex-row-fluid">
-
-              <div class="row g-10"> 
-                <div class="d-flex flex-column ">
-                    <div class="d-flex flex-center flex-column flex-column-fluid p-5 pb-lg-20">                       
-                    <div class="d_flex p-2">
-                                
-                        <a href="{{ url('/') }}">
-                            <img alt="Logo" src="assets/media/logos/logo.png" class="h-60px text-center nb-2 me-4" />
-                        </a>
-                        
-                        <h3 class="text-center text-white text-uppercase fs-2">Welcome to Task Sprint</h3>
-                        
-                                
-                            
+            <div class="d-flex flex-column flex-column-fluid flex-center w-lg-50 p-10">
+                <div class="d-flex justify-content-between  flex-column w-100 mw-450px">
+                    <div class="d-flex flex-stack py-2">
+                        <div class="me-2">
+                          <a href="{{ url('/') }}">
+                              <img alt="Logo" src="assets/media/logos/logo.png" class="h-60px text-center nb-2 me-4" />
+                          </a>
                         </div>
-                        <div class="w-lg-500px bg-body rounded shadow-sm p-5 p-lg-10 mx-auto">
-                            <form method="POST" action="{{ route('login') }}" class="form w-100 fv-plugins-bootstrap5 fv-plugins-framework">
-                                @csrf   
+
+                        <div class="m-0">
+                          <h3 class="text-center text-uppercase fs-2">Welcome to Task Sprint</h3>
+
+                        </div>
+                    </div>
+
+                    <div class="py-10">
+                        <form method="POST" action="{{ route('login') }}" class="form w-100 fv-plugins-bootstrap5 fv-plugins-framework">
+                            @csrf   
+                            <div class="card-body">
+                                <div class="text-start mb-10">
+                                    <h1 class="text-dark mb-3 fs-3x" data-kt-translate="sign-in-title">
+                                        Sign In
+                                    </h1>
+
+                                    <div class="text-gray-400  fs-6" data-kt-translate="general-desc">
+                                        Working with experienced teams makes work easier & gets you closer to your goals.
+                                    </div>
+                                </div>
+
                                 <div class="fv-row mb-10 fv-plugins-icon-container fv-plugins-bootstrap5-row-valid">
-                                    <label class="form-label fs-6  text-dark">Email</label>
-                                    <input id="email" type="email" class="form-control form-control-lg form-control-solid @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                   
+                                    <input id="email" type="email" class="form-control form-control-lg form-control-solid @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" autofocus>
 
                                     @error('email')
                                         <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback" role="alert">{{ $message }}</div>
                                     @enderror
                                 </div>
-
                                 <div class="fv-row mb-10 fv-plugins-icon-container fv-plugins-bootstrap5-row-valid">
                                     <div class="d-flex flex-stack mb-2">
                                         <label class="form-label  text-dark fs-6 mb-0">Password</label>
@@ -237,99 +99,64 @@
                                         <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback" role="alert">{{ $message }}</div>
                                     @enderror
                                 </div>
+                           
+                                <div class="d-flex flex-stack">
+                                    <button id="kt_sign_in_submit" class="btn btn-primary me-2 flex-shrink-0">
+                                        <span class="indicator-label" data-kt-translate="sign-in-submit">
+                                          {{ __('Login') }}
+                                        </span>
 
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-lg btn-primary w-100 mb-5">
-                                        <span class="indicator-label">
-                                            {{ __('Login') }}
+                                        <span class="indicator-progress">
+                                            <span data-kt-translate="general-progress">Please wait...</span>
+                                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
                                         </span>
                                     </button>
 
-                                    <div class="text-center text-muted text-uppercase mb-2">or</div>
+                                    <div class="d-flex align-items-center">
+                                        <div class="text-gray-400  fs-6 me-3 me-md-6" data-kt-translate="general-or">Or</div>
 
-                                    <div class="">
-                                        <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
-                                            <img alt="Logo" src="assets/media/svg/brand-logos/google-icon.svg" class="h-20px me-3" />
-                                            Continue with Google
-                                        </a>
-                                        <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
-                                            <img alt="Logo" src="assets/media/svg/brand-logos/github.svg" class="theme-light-show h-20px me-3" />                                           
-                                            Continue with Github
+                                        <a href="#" class="symbol symbol-circle symbol-45px w-45px bg-light me-3">
+                                            <img alt="Logo" src="assets/media/svg/brand-logos/google-icon.svg" class="p-4" />
                                         </a>
 
+                                        <a href="#" class="symbol symbol-circle symbol-45px w-45px bg-light me-3">
+                                            <img alt="Logo" src="assets/media/svg/brand-logos/facebook-3.svg" class="p-4" />
+                                        </a>
 
+                                        <a href="#" class="symbol symbol-circle symbol-45px w-45px bg-light">
+                                            <img alt="Logo" src="assets/media/svg/brand-logos/apple-black.svg" class="theme-light-show p-4" />
+                                            <img alt="Logo" src="assets/media/svg/brand-logos/apple-black-dark.svg" class="theme-dark-show p-4" />
+                                        </a>
                                     </div>
-
-
-                                    
-                                    <div class="text-center text-muted text-uppercase mb-2 mt-4">or</div>
-
-                                    <div class="text-center mb-10 mt-2">
-                                        
-                                    <div class="text-gray-400 fs-4">
-                                            New Here?
-
-                                            <a href="#" class="link-primary ">
-                                                Create an Account
-                                            </a>
-                                        </div>
-                                    </div>
-
-
                                 </div>
-                            </form>
-                        </div>
+                                <br/>
+                                <br/>
+                                <div class="mt-4">
+                                  <span class="text-gray-400  fs-5 me-2" data-kt-translate="sign-in-head-desc">
+                                      Not a Member yet?
+                                  </span>
+                                  @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="link-primary  fs-5" data-kt-translate="sign-in-head-link">
+                                        Sign Up
+                                    </a>
+                                  @endif
+                              </div>
+                            </div>
+                        </form>
                     </div>
+                </div>
+            </div>
 
-                    <div class="d-flex flex-center flex-column-auto mb-2 mt-0">
-                        <div class="d-flex align-items-center  fs-6">
-                            <a href="#" class="text-muted text-hover-primary px-2">About</a>
-
-                            <a href="#" class="text-muted text-hover-primary px-2">Support</a>
-
-                            <a href="#" class="text-muted text-hover-primary px-2">
-                                Purchase
-                            </a>
-                        </div>
-                    </div>
-                </div>              
-              
-              </div>
-
+            <div class="d-none d-lg-flex flex-lg-row-fluid w-50 bgi-size-cover bgi-position-y-center bgi-position-x-start bgi-no-repeat" style="background-image: url(assets/media/images/login-bg.svg);"></div>
         </div>
-
-      </div>
-    </div>
-
-    <div class="engage-toolbar d-flex position-fixed px-5 zindex-2 top-50 end-0 transform-90 mt-5 mt-lg-20 gap-2"></div>
-
-    <div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
-      <i class="bi bi-arrow-up"><span class="path1"></span><span class="path2"></span></i>
-    </div>
-
-
-    
-
-
-    <div class="area">
-      <ul class="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
     </div>
 
     <script>
-      var hostUrl = "assets/";
+        var hostUrl = "assets/";
     </script>
 
+    <script src="assets/plugins/global/plugins.bundle.js"></script>
+    <script src="assets/js/scripts.bundle.js"></script>
+</body>
 
-  </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,77 +1,137 @@
 <!DOCTYPE html>
-
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <title>Task Sprint</title>
-    <meta charset="utf-8" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.png" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/main.css" rel="stylesheet" type="text/css" />
-  </head>
+
+<head>
+  <title>Task Sprint</title>
+  <meta charset="utf-8" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" href="assets/media/logos/favicon.png" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+  <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="assets/css/main.css" rel="stylesheet" type="text/css" />
 
   <style>
-    body{
-      background: transparent linear-gradient(90deg, #14EB8F 0%, #00B78A 100%) 0% 0% no-repeat padding-box;
+    * {
+      font-family: "Roboto", sans-serif;
+      box-sizing: border-box;
     }
- /* .page-bg {
-        background-size: cover;
-        background-attachment: fixed;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-image: url('assets/media/images/bg.jpg');
-    } */
+
+    body {
+      margin: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #e9ebf1;
+    }
+
+    .wrapper {
+      width: 100%;
+      max-width: 400px;
+      height: 640px;
+      position: relative;
+      overflow: hidden;
+      background: #fff;
+      border-radius: 4px;
+      /* box-shadow: 0 10px 20px 20px rgba(0, 0, 0, 0.08); */
+    }
+
+    .home {
+      padding: 40px;
+    }
+
+    .welcome-image {
+      max-width: 100%;
+      height: auto;
+      display: block;
+      margin-bottom: 20px;
+    }
+
+    .heading {
+      color: #6f2d80;
+      font-size: 23px;
+      text-align: center;
+      font-weight: 700;
+    }
+
+    .sub-heading {
+      color: #99a1b7;
+      font-size: 20px;
+      text-align: center;
+      margin-top: 20px;
+    }
+
+    .btn {
+      color: white;
+      background: black;
+      text-transform: uppercase;
+      border: 0;
+      padding: 15px 40px;
+      border-radius: 4px;
+      font-weight: 700;
+      letter-spacing: 2px;
+      cursor: pointer;
+      display: block;
+      margin: 30px auto;
+      transition: background-color 0.3s ease;
+    }
+
+    .btn:hover {
+      background-color: #6f2d80;
+    }
+
+    @media (max-width: 500px) {
+      .heading {
+        font-size: 20px;
+      }
+
+      .sub-heading {
+        font-size: 16px;
+      }
+
+      .btn {
+        font-size: 14px;
+        padding: 10px 30px;
+      }
+    }
+
+    @keyframes fadeInUp {
+      from {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+
+    .home {
+      animation: fadeInUp 0.8s ease;
+    }
   </style>
-<body id="kt_body" class="app-blank bgi-size-cover bgi-position-center bgi-no-repeat bg-primary">
-  <div class="d-flex flex-column flex-root" id="kt_app_root">
-    <div class="mt-4">
-        <div class="d-flex flex-column flex-center text-center p-5">
-            <div class="card card-flush w-md-650px py-2 bg-white">
-                <div class="card-body py-10 py-lg-20">
-                    <div class="mb-7">
-                        <a href="{{ route('home') }}" class="">
-                            <img alt="Logo" src="assets/media/logos/logo.png" class="h-40px" />
-                        </a>
-                    </div>
+</head>
 
-                    <h1 class="fw-bolder text-gray-900 mb-4">
-                        Welcome to Task Sprint
-                    </h1>
-
-                    <div class="fw-semibold fs-6 text-gray-500 mb-4">
-                     
-
-                      Welcome to Task Sprint, fueled by cutting-edge AI technology! <br/> Ready to supercharge your productivity? <br/> Let's get started! 🚀
-                     </div>
-
-                    <div class="mb-4">
-                        <img src="assets/media/images/work-1.png" class="mw-100 mh-300px theme-light-show" alt="welcome" />
-                        
-                    </div>
-
-                    <div class="mb-0">
-                        <a href="{{ route('login') }}" class="btn btn-sm btn-label-primary">Go To Dashboard</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+<body id="kt_body" class="app-blank page-bg">
+  <div class="wrapper">
+    <div class="home">
+      <img src="assets/media/images/work-1.png" alt="image" class="welcome-image" />
+      <h1 class="heading mt-5">Let's Work Together</h1>
+      <h5 class="sub-heading fs-6 fw-semibold text-muted mb-10">Working with experienced teams makes work easier & gets you closer to your goals.</h5>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <a class="btn btn-default bottom-0" href="{{ route('login') }}">Get Started</a>
     </div>
-</div>
+  </div>
+  <script src="assets/js/custom.js"></script>
+</body>
 
-
-    
-    <script>
-      var hostUrl = "assets/";
-    </script>
-
-    <script src="assets/plugins/global/plugins.bundle.js"></script>
-    <script src="assets/js/scripts.bundle.js"></script>
-
-  </body>
 </html>
